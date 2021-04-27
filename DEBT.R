8d35c3aee6b572e5e64c4264791aa160f171320e
AnnKari <79765917+AnnKari@users.noreply.github.com>
2021-04-26 20:11:19 -0500

    Update DEBT.R

--- DEBT.R
+++ DEBT.R
@@ -1,19 +1,3 @@
-################################################################################
-################################################################################
-####################       Economic  Crisis     ################################
-####################       Impact on Rates      ################################
-####################              and           ################################
-####################       Internal Debt        ################################
-####################     Data Visualization     ################################
-################################################################################
-################################################################################
-###################     Anna Karina P√©rez Pe√±a      ##########################
-###################    Sebasti√°n Ocampo Palacios    ###########################
-################################################################################
---------------------------------------------------------------------------------
-
-# CODE -------------------------------------------------------------------
-
 # 1. Libraries & data -----------------------------------------------------
 
 library(readxl)
@@ -23,8 +7,8 @@ library(dygraphs)
 library(seasonal)
 library(ggplot2)
 
-
-setwd("C:/Users/socap/OneDrive/Documentos/GitHub/crisis-debt-rates")
+rm(list=ls())
+setwd("C:/Users/secun/OneDrive/Documentos/6to/Money/Data Vizualization Project")
 
 DATA=read_xlsx("DEBT85-20.xlsx", col_names=FALSE, na="NA")
 DATA=DATA[2:434,] #Removes headers
@@ -52,44 +36,44 @@ DATA$Dates=dates #Redifines date data.
 summary(DATA)
 
 #Obtaining real values for debt.
-  base= 109.27100 #INPC, December 2015 as base year.
-                  # Alternative is 99.90910
-  DATA$INPC=DATA$INPC/base *100 #We create the deflator coefficient
+base= 109.27100 #INPC, December 2015 as base year.
+# Alternative is 99.90910
+DATA$INPC=DATA$INPC/base *100 #We create the deflator coefficient
 
-  DATA$INTERNAL_DEBT=DATA$INTERNAL_DEBT/DATA$INPC #Deflated Debt
-  
+DATA$INTERNAL_DEBT=DATA$INTERNAL_DEBT/DATA$INPC #Deflated Debt
+DATA$AVG_TIIE91=DATA$AVG_TIIE91/DATA$INPC
 #Obtaining real values for the Interest Rates. 
-  #data$AVG_TIIE91=data$AVG_TIIE91/data$INPC
+#data$AVG_TIIE91=data$AVG_TIIE91/data$INPC
 
-  
- 
+
+
 # 2. Time Series  ---------------------------------------------------------
-  
-  # 1997-2020. 
-  dates=dates[145:432]
-  
-  #We need to deseasonalize our data using the package "seasonal".
-  DATA=DATA[145:432,] # Timeframe 1997-2020.
-  DATA=DATA[c("INTERNAL_DEBT", "AVG_TIIE91")] #Reduces variables
-  
-  #Deseasonalize
-  Des_data <- ts(coredata(DATA),frequency=12,start=c(1997,1), end = c(2020, 12))
-  inData <- seas(Des_data) #seas function.
-  Des_data=as.data.frame(inData)
+
+# 1997-2020. 
+dates=dates[145:432]
+
+#We need to deseasonalize our data using the package "seasonal".
+DATA=DATA[145:432,] # Timeframe 1997-2020.
+DATA=DATA[c("INTERNAL_DEBT", "AVG_TIIE91")] #Reduces variables
+
+#Deseasonalize
+Des_data <- ts(coredata(DATA),frequency=12,start=c(1997,1), end = c(2020, 12))
+inData <- seas(Des_data) #seas function.
+Des_data=as.data.frame(inData)
 
 #?
 #DATA<-cbind(DATA$INTERNAL_DEBT,DATA$AVG_TIIE91) 
 #colnames(DATA) <-c("internal debt","interest rate") 
-#plot(DATA, main="Internal rate and interest rate",xlab="AÒo",ylab="$")
+#plot(DATA, main="Internal rate and interest rate",xlab="A√±o",ylab="$")
 #Data = as.data.frame(DATA)
 
 
 #We add the seasonal adjusted data to our data.
 DATA<-cbind(dates, DATA, Des_data$INTERNAL_DEBT.seasonaladj, Des_data$AVG_TIIE91.seasonaladj) 
 colnames(DATA)= c("dates", "INTERNAL_DEBT", "AVG_TIIE91", "INTERNAL_DEBT_SA", "AVG_TIIE91_SA")
-  
-  
-  
+
+
+
 # 3. Graphs ---------------------------------------------------------------
 
 #ggplot2 - General graph----------------------------
@@ -116,7 +100,7 @@ plot(x=DATA$date,y=DATA$AVG_TIIE91_SA,type="l") #Seasonal Adjusted
 #   
 #   theme_ipsum()
 
-coeff <- as.numeric(1000000)
+coeff <- as.numeric(100000000)
 rateColor <- "#69b3a2"
 debtColor <- rgb(0.2, 0.6, 0.9, 1)
 
@@ -132,7 +116,7 @@ ggplot(DATA, aes(x=dates))+
     sec.axis = sec_axis(~.*coeff, name="Internal Debt")
   ) +
   
-  #theme_ipsum() +  No est· funcionando esta lÌnea :( 
+  #theme_ipsum() +  No est√° funcionando esta l√≠nea :( 
   
   theme(
     axis.title.y = element_text(color = rateColor, size=13),
@@ -164,36 +148,43 @@ dygraph(DATA_TS[,3:4], main= "Debt!") %>%
 
 #We subset by years.
 crisis_08=DATA_TS[c("2007","2008", "2009", "2010", "2011")]
-  dates_08=seq(from=as.Date("2007-01-01"), to=as.Date("2011-12-01"), by="month")
+dates_08=seq(from=as.Date("2007-01-01"), to=as.Date("2011-12-01"), by="month")
+crisis_08$INTERNAL_DEBT_EE=(as.numeric(crisis_08$INTERNAL_DEBT_SA)/30574098)-1
+crisis_08$AVG_TIIE91_EE=(crisis_08$AVG_TIIE91_SA/0.14143449)-1
 
 crisis_20=DATA_TS[c("2019", "2020")]
-  dates_20=seq(from=as.Date("2019-01-01"), to=as.Date("2020-12-01"), by="month")
+dates_20=seq(from=as.Date("2019-01-01"), to=as.Date("2020-12-01"), by="month")
+crisis_20$INTERNAL_DEBT_EE=(as.numeric(crisis_20$INTERNAL_DEBT_SA)/60380768)-1
+crisis_20$AVG_TIIE91_EE=(crisis_20$AVG_TIIE91_SA/0.06998935)-1
 
-  #We want to plot the year before the beginning of each crisis and the three years afterwards.
-T=1:60 #5 years= 60 months.
-
+#We want to plot the year before the beginning of each crisis and the three years afterwards.
+#T=1:60 #5 years= 60 months.
 #We pair dates to each month's position.
+T=-19:40
 crisis_08$T=T
-crisis_20$T=T[1:nrow(crisis_20)] #24 obs. for the 2020 crisis.
+
+T=-14:9
+#crisis_20$T=T[1:nrow(crisis_20)] #24 obs. for the 2020 crisis.
+crisis_20$T=T
 
 #Disband xts
 crisis_08=(as.data.frame(crisis_08))
 crisis_20=(as.data.frame(crisis_20))
 
 #Compensating shorter data frame
-crisis_20=rbind(crisis_20, crisis_20[25:60, ])
-crisis_20$T=T #60 obs. for the 2020 crisis.
+#crisis_20=rbind(crisis_20, crisis_20[25:60, ])
+#crisis_20$T=T #60 obs. for the 2020 crisis.
 
 #Graph
-coeff <- as.numeric(1000000)
+coeff <- as.numeric(1)#100000000
 rateColor <- "#69b3a2"
 debtColor <- rgb(0.2, 0.6, 0.9, 1)
 
 
 #2008
 ggplot(crisis_08, aes(x=T))+
-  geom_line( aes(y=as.numeric(AVG_TIIE91_SA)), size=1, color=rateColor) + #2008 TIIE
-  geom_line( aes(y=INTERNAL_DEBT_SA/coeff), size=1, color=debtColor) +  #2008 DEBT
+  geom_line( aes(y=as.numeric(AVG_TIIE91_EE)), size=1, color=rateColor) + #2008 TIIE
+  geom_line( aes(y=INTERNAL_DEBT_EE/coeff), size=1, color=debtColor) +  #2008 DEBT
   
   scale_y_continuous(
     # Features of the first axis
@@ -203,33 +194,7 @@ ggplot(crisis_08, aes(x=T))+
     sec.axis = sec_axis(~.*coeff, name="Internal Debt")
   ) +
   
-  #theme_ipsum() +  No est· funcionando esta lÌnea :( 
-  
-  theme(
-    axis.title.y = element_text(color = rateColor, size=13),
-    axis.title.y.right = element_text(color = debtColor, size=13)
-  ) +
-  
-  ggtitle("Title") +
- # scale_x_date(date_labels = "%Y-%m", date_breaks = "2 year") +
-  theme(axis.text.x=element_text(angle=60, hjust=1))
-
-
-
-#2020
-ggplot(crisis_20, aes(x=T))+
-  geom_line( aes(y=as.numeric(AVG_TIIE91_SA)), size=1, color=rateColor) + #2008 TIIE
-  geom_line( aes(y=INTERNAL_DEBT_SA/coeff), size=1, color=debtColor) +  #2008 DEBT
-  
-  scale_y_continuous(
-    # Features of the first axis
-    name = "Interest Rate",
-    
-    # Add a second axis and specify its features
-    sec.axis = sec_axis(~.*coeff, name="Internal Debt")
-  ) +
-  
-  #theme_ipsum() +  No est· funcionando esta lÌnea :( 
+  theme_ipsum() +  #No est√° funcionando esta l√≠nea :( 
   
   theme(
     axis.title.y = element_text(color = rateColor, size=13),
@@ -239,6 +204,41 @@ ggplot(crisis_20, aes(x=T))+
   ggtitle("Title") +
   # scale_x_date(date_labels = "%Y-%m", date_breaks = "2 year") +
   theme(axis.text.x=element_text(angle=60, hjust=1))
+
+
+
+#2020
+
+ggplot(crisis_20, aes(x=T))+
+  geom_line( aes(y=as.numeric(AVG_TIIE91_EE)), size=1, color=rateColor) + #2008 TIIE
+  geom_line( aes(y=INTERNAL_DEBT_EE/coeff), size=1, color=debtColor) +  #2008 DEBT
+  
+  scale_y_continuous(
+    # Features of the first axis
+    name = "Interest Rate",
+    
+    # Add a second axis and specify its features
+    sec.axis = sec_axis(~.*coeff, name="Internal Debt")
+  ) +
+  
+ theme_ipsum() +  #No est√° funcionando esta l√≠nea :( 
+  
+  theme(
+    axis.title.y = element_text(color = rateColor, size=13),
+    axis.title.y.right = element_text(color = debtColor, size=13)
+  ) +
+  
+  ggtitle("Title") +
+  # scale_x_date(date_labels = "%Y-%m", date_breaks = "2 year") +
+  theme(axis.text.x=element_text(angle=60, hjust=1))
+
+
+
+
+
+
+
+
 
 
 
